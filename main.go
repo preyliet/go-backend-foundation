d9be5f32fabef072fbaa77507ed16c9859531c92
Rohan <iix.preyliet@gmail.com>
2026-01-29 05:37:11 +0530

    day 1: go basics, variables, zero values, notes

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variable of type uint8
+
+	var smalPositiveValue uint8
+	smalPositiveValue = 20
+	fmt.Println(smalPositiveValue)
+
+	// Variable of type int8
+
+	var smalNegativeValue int8
+	smalNegativeValue = -12
+	fmt.Println(smalNegativeValue)
+
+}
